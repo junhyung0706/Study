f5caef0976544d216e07afccdb69a8d1d9309c00
junhyung <72088449+junhyung0706@users.noreply.github.com>
2024-02-14 15:57:07 +0900

    Add files via upload

---
+++ Cpp_Practice/MatrixProduct.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[3][3] =
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int b[3][3] =
+    {
+        {2, 1, 3},
+        {4, 5, 7},
+        {6, 9, 8}
+    };
+    int c[3][3];
+
+    int sum;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            sum = 0;
+            for (int k = 0; k < 3; k++)
+            {
+                sum += a[i][k] * b[k][j];
+            }
+            c[i][j] = sum;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
