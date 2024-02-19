cc50160f54cb22170ecb77da385f83e4de95d179
junhyung <72088449+junhyung0706@users.noreply.github.com>
2024-02-19 14:19:13 +0900

    Update rgb_fsm.v

--- Verilog_Practice/FSM으로 만든 신호등1/rgb_fsm.v
+++ Verilog_Practice/FSM으로 만든 신호등1/rgb_fsm.v
@@ -12,10 +12,10 @@ module rgb_fsm(
         reg [1:0] push_reg;
         wire [1:0] push_button;
      
-		parameter S0 = 2'b00;
-		parameter S1 = 2'b01;
-		parameter S2 = 2'b10;
-		parameter S3 = 2'b11;
+	parameter S0 = 2'b00;
+	parameter S1 = 2'b01;
+	parameter S2 = 2'b10;
+	parameter S3 = 2'b11;
         reg [31:0] clkcnt=0;
         
         always @(posedge clk) begin
@@ -26,50 +26,50 @@ module rgb_fsm(
         assign push_button[1] = push[1] & ~push_reg[1];
 
     //State Transition
-		always @(negedge resetn or posedge clk) begin
-			if ( !resetn )
-				state <= S0;
-			else if ( push_button == 2'b01 ) begin
-			case ( state )
-                S0 : state <= S1;
-                S1 : state <= S2;
-                S2 : state <= S1;
-				default : state <= state;
-			endcase
-			end
-			else if ( push_button == 2'b10 ) begin
-			case ( state )
-	           S0 : state <= S3;
-	           S1 : state <= S3;
-	           S2 : state <= S3;
-	           S3 : state <= S1;
-				default : state <= state;
-			endcase
-			end
-			else
-				state <= state;
+	always @(negedge resetn or posedge clk) begin
+		if ( !resetn )
+			state <= S0;
+		else if ( push_button == 2'b01 ) begin
+		case ( state )
+	S0 : state <= S1;
+	S1 : state <= S2;
+	S2 : state <= S1;
+			default : state <= state;
+		endcase
 		end
+		else if ( push_button == 2'b10 ) begin
+		case ( state )
+	   S0 : state <= S3;
+	   S1 : state <= S3;
+	   S2 : state <= S3;
+	   S3 : state <= S1;
+			default : state <= state;
+		endcase
+		end
+		else
+			state <= state;
+	end
 
-		//Output Operation
-		always @(posedge clk) begin
-		    clkcnt <= clkcnt + 1;
-		    if (clkcnt > 32'd130_000_000 * 2 - 1) begin
-		        clkcnt <= 0;
-		    end
-			case ( state )
-	           S0 : led_output <= 6'b111_111;
-	           S1 : led_output <= 6'b010_100;
-	           S2 : led_output <= 6'b100_010;
-	           S3 : begin
-	               if (clkcnt < 32'd130_000_000) begin
-	                   led_output <= 6'd000_000;
-                   end
-                   else begin
-                       led_output <= 6'd110_110;
-                   end
-               end
-				default : led_output <= 6'b000000;
-			endcase
-		end
+	//Output Operation
+	always @(posedge clk) begin
+	    clkcnt <= clkcnt + 1;
+	    if (clkcnt > 32'd130_000_000 * 2 - 1) begin
+		clkcnt <= 0;
+	    end
+		case ( state )
+	   S0 : led_output <= 6'b111_111;
+	   S1 : led_output <= 6'b010_100;
+	   S2 : led_output <= 6'b100_010;
+	   S3 : begin
+	       if (clkcnt < 32'd130_000_000) begin
+		   led_output <= 6'd000_000;
+	   end
+	   else begin
+	       led_output <= 6'd110_110;
+	   end
+       end
+			default : led_output <= 6'b000000;
+		endcase
+	end
 		
 endmodule
