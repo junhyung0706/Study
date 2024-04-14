265d2991ef23577281c3bc3214dfb08a6c855f97
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-04-14 10:09:54 +0900

    Update ring_counter.v

--- Verilog_Practice/ring_counter.v
+++ Verilog_Practice/ring_counter.v
@@ -1,17 +1,17 @@
 module ring_counter (count, enable, clock, reset);
-  output reg [7:0] count;
-  input, enable, clock, reset;
+    output reg [7:0] count;
+    input, enable, clock, reset;
 
-  always @(posedge clock, posedge reset)
-    if(reset == 1'b1) count <= 8'b0000_0001;
-    else if (enable == 1'b1) begin
-        case (count)
-            8'b0000_0001: count <= 8'b0000_0010;
-            8’b0000_0010: count <= 8’b0000_0100;
-            // ...
-            8’b1000_0000: count <= 8’b0000_0001;
-            default: count <= 8’bxxxx_xxxx; 
-        endcase
-    end
+    always @(posedge clock, posedge reset)
+        if(reset == 1'b1) count <= 8'b0000_0001;
+        else if (enable == 1'b1) begin
+            case (count)
+                8'b0000_0001: count <= 8'b0000_0010;
+                8’b0000_0010: count <= 8’b0000_0100;
+                // ...
+                8’b1000_0000: count <= 8’b0000_0001;
+                default: count <= 8’bxxxx_xxxx; 
+            endcase
+        end
     
 endmodule
