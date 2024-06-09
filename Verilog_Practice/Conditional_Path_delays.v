7d75d376cc09618037f6676b93f97737cb894399
말랑핫도그 <72088449+junhyung0706@users.noreply.github.com>
2024-06-09 23:16:08 +0900

    Update Conditional_Path_delays.v

--- Verilog_Practice/Conditional_Path_delays.v
+++ Verilog_Practice/Conditional_Path_delays.v
@@ -1,16 +1,20 @@
 module M(out,a,b,c,d);
-output out;
-input a,b,c,d;
-wire e,f;
-specify
-    if(a) (a=>out)=9;
-    if(~a) (a=>out)=10;
-    if(b&c)(b=>out)=9;
-    if(~(b&c)) (b=>out)=13;
-    if({c,d}==2'b01) (c,d*>out)=11;
-    if({c,d}!=2'b01) (c,d*>out)=13;
-endspecify
-and a1(e,a,b);
-and a2(f,c,d);
-and a3(out,e,f);
+    
+    output out;
+    input a,b,c,d;
+    wire e,f;
+    
+    specify
+        if(a) (a=>out)=9;
+        if(~a) (a=>out)=10;
+        if(b&c)(b=>out)=9;
+        if(~(b&c)) (b=>out)=13;
+        if({c,d}==2'b01) (c,d*>out)=11;
+        if({c,d}!=2'b01) (c,d*>out)=13;
+    endspecify
+        
+    and a1(e,a,b);
+    and a2(f,c,d);
+    and a3(out,e,f);
+        
 endmodule
